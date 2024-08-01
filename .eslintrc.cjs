843b7af76552725884d6c9ef8516c8ea1866b39b
pulak-sarmah <pulaksarmah333@gmail.com>
2024-08-01 21:02:18 +0530

    chore: Update eslint configuration to disable specific rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,7 @@ module.exports = {
         "no-console": "error",
         "dot-notation": "error",
         "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/unbound-method": "off",
     },
 };
