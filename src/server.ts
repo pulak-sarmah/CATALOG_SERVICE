5fe1162fa8635794df4d08f63470f835e9d7b2cb
pulak-sarmah <pulaksarmah333@gmail.com>
2024-08-01 11:47:02 +0530

    chore: Update database configuration for different environments

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import config from "config";
 import app from "./app";
 import logger from "./config/logger";
+import { initDb } from "./config/db";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT: number = config.get("server.port") || 5502;
     try {
+        await initDb();
+
+        logger.info("Database connected successfully");
+
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -16,4 +21,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
