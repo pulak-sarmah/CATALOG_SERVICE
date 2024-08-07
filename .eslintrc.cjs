53a2754a8bfadcaa9ccc1acd5f9a8952f72ba086
pulak-sarmah <pulaksarmah333@gmail.com>
2024-08-07 09:42:57 +0530

    chore: Update ESLint configuration to ignore .cjs files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
         "@typescript-eslint/require-await": "off",
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
     },
 };
